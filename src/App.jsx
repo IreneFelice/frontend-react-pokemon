7661f8a91a97101763e226c36a09ebf35fe943b4
IreneFelice <i.f.buijing@gmail.com>
2024-01-15 10:08:29 +0100

    uitwerkingen branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import './App.css'
 
 function App() {
+    const [facts, setFacts] = useState([]);
+
+    async function fetchData(){
+        try{
+            const response = await
+                axios.get('https://pokeapi.co/api/v2/pokemon/ditto',
+                );
+            setFacts(response.data);
+        } catch(error){
+            console.log(error);
+        }
+    }
+
+    useEffect(()=>{
+            fetchData();
+            console.log('pokemon');
+    },[]);
 
   return (
     <>
       <h1>Gotta catch em all!</h1>
+        <div>{facts.map((el,i) =>
+        {
+            return (
+                <div key={i}>
+                    {el.fact}
+                </div>
+            );
+        })}</div>
     </>
   )
 }
+
+toevoegen nieuwe branch
 
 export default App
