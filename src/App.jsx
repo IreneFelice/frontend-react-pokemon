7ee58385c72d3da71e0c6d9d4911d231b604d379
IreneFelice <i.f.buijing@gmail.com>
2024-01-15 14:34:46 +0100

    20 poké cards according format

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,50 @@
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import PokemonCard from './components/PokemonCard.jsx';
 
 function App() {
-    const [facts, setFacts] = useState([]);
+    const [pokemonDataList, setPokemonDataList] = useState([]);
 
-    async function fetchData(){
-        try{
-            const response = await
-                axios.get('https://pokeapi.co/api/v2/pokemon/ditto',
-                );
-            setFacts(response.data);
-        } catch(error){
+    async function catchPokeNames() {
+        try {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+            const pokemonNames = response.data.results.map((pokemon) => pokemon.name );
+            return pokemonNames;
+        } catch (error) {
             console.log(error);
         }
     }
 
-    useEffect(()=>{
-            fetchData();
-            console.log('pokemon');
-    },[]);
+    async function catchPokeData(name) {
+        try {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+            setPokemonDataList((prevDataList) => [...prevDataList, response.data]);
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+        }
 
-  return (
-    <>
-      <h1>Gotta catch em all!</h1>
-        <div>{facts.map((el,i) =>
-        {
-            return (
-                <div key={i}>
-                    {el.fact}
-                </div>
-            );
-        })}</div>
-    </>
-  )
+
+    useEffect(() => {
+        async function fetchData() {
+            const pokemonNames = await catchPokeNames();
+            pokemonNames.forEach((name) => {
+                catchPokeData(name);
+            });
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <h1>Gotta catch em all!</h1>
+            {pokemonDataList.map((pokemonData, index) => (
+                <PokemonCard key={index} pokemonData={pokemonData} />
+            ))}
+        </>
+    );
 }
 
-toevoegen nieuwe branch
-
-export default App
+export default App;
